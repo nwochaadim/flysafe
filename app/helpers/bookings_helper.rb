1b2be2c792d13f3eeee3e95d208fbeb52806e3cb
andela-anwocha <adimoranma.felix@andela.com>
2016-06-21 04:16:52 +0100

    feat(booking page): Design booking page

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,23 @@
 module BookingsHelper
+  def calculate_flight_fare(booking_params)
+    no_of_adults = booking_params["adult"] ? booking_params["adult"].count + 1 : 1
+    no_of_children = booking_params["child"] ? booking_params["child"].count : 0
+    no_of_infants = booking_params["child"] ? booking_params["infant"].count  : 0
+    total_cost = no_of_adults * (AdultPassenger::ECONOMY_FLIGHT_FARE) + 
+                  no_of_children * (ChildPassenger::ECONOMY_FLIGHT_FARE) +
+                  no_of_infants * (InfantPassenger::ECONOMY_FLIGHT_FARE)
+  end
+
+  def adult_fare
+    "$#{AdultPassenger::ECONOMY_FLIGHT_FARE}"
+  end
+
+  def child_fare
+    "$#{ChildPassenger::ECONOMY_FLIGHT_FARE}"
+  end
+
+  def infant_fare
+    "$#{InfantPassenger::ECONOMY_FLIGHT_FARE}"
+  end
+
 end
